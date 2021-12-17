5a28528eee06d8a61b5b30d0e1183cc9b0950228
Mridul <gmridul556@gmail.com>
2021-12-17 15:11:07 +0530

    images added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
                 price : 999, 
                 title : 'Mobile Phone',
                 qty : 1,
-                img : '',
+                img : 'https://images.unsplash.com/photo-1585060544812-6b45742d762f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW9iaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
                 id: 1
                 
             },
@@ -21,14 +21,14 @@ class App extends React.Component {
                 price : 99999, 
                 title : 'Laptop',
                 qty : 2,
-                img : '',
+                img : 'https://media.istockphoto.com/photos/laptop-computer-with-blue-pink-lighting-and-blank-screen-place-on-picture-id1292038829?b=1&k=20&m=1292038829&s=170667a&w=0&h=L8DhcSOFS3BZdCHN6ldjumqQBw0J2kk7mBagO5io5jM=',
                 id: 2
             },
             {
                 price : 99, 
                 title : 'Watch',
                 qty : 5,
-                img : '',
+                img : 'https://images.unsplash.com/photo-1523170335258-f5ed11844a49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2F0Y2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
                 id: 3
             }
         ]
